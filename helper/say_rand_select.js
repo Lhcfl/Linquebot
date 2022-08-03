5257998c6654a0b36693524c16482fbf3c430823
Virginia Senioria <91khr@users.noreply.github.com>
2022-08-03 10:14:05 +0800

    refa say_rand_select: standalone possibility

--- helper/say_rand_select.js
+++ helper/say_rand_select.js
@@ -2,13 +2,13 @@
  * Iterate msglist, each has possibility to be selected, and say the selected one.
  *
  * @param {(msg: Sendable, delay?: number) => void} msg_say The function to say a message
- * @param {([Sendable, number?] | () => [Sendable, number?])[]} msglist The list of messages to be selected
- * @param {number} possibility The possibility of each message to be selected.
+ * @param {[number, ([Sendable, number?] | () => [Sendable, number?])][]} msglist The list of messages to be selected,
+ * where the first item is the possibility to be selected, the second is the message.
  *
  * @returns {boolean} Whether said a message
  */
-export function say_rand_select(msg_say, msglist, possibility) {
-    for (const msg of msglist) {
+export function say_rand_select(msg_say, msglist) {
+    for (const [possibility, msg] of msglist) {
         if (Math.random() < possibility) {
             let send = msg instanceof Function ? msg() : msg;
             msg_say.apply(msg_say, send);
--- tests/helper/say_rand_select.js
+++ tests/helper/say_rand_select.js
@@ -19,18 +19,18 @@ test("say something", () => {
     let p;
     Math.random = () => p;
     p = 1;
-    assert.strictEqual(say_rand_select(check_eq(0, 1), data, 2), true);
+    assert.strictEqual(say_rand_select(check_eq(0, 1), data.map(v => [2, v])), true);
     p = 2;
-    assert.strictEqual(say_rand_select(check_eq(4, 5), data, 2), false);
+    assert.strictEqual(say_rand_select(check_eq(4, 5), data.map(v => [2, v])), false);
 
     Math.random = () => {
         p = p - 1;
         return p;
     };
     p = 9;  // 9 - 4 = 5
-    assert.strictEqual(say_rand_select(check_eq(5, 6), data, 4), true);
+    assert.strictEqual(say_rand_select(check_eq(5, 6), data.map(v => [4, v])), true);
     p = 10;  // 10 - 0 = 10, not found
-    assert.strictEqual(say_rand_select(check_eq(5, 6), data, 0), false);
+    assert.strictEqual(say_rand_select(check_eq(5, 6), data.map(v => [0, v])), false);
 });
 
 test("test function", () => {
@@ -38,5 +38,5 @@ test("test function", () => {
     say_rand_select((a, b) => {
         if (a != 0 || b != 1)
             throw Error(`not eq: expect 0, 1, got ${a}, ${b}`);
-    }, [() => [0, 1], [1, 2], [2, 3]], 1);
+    }, [[1, () => [0, 1]], [1, [1, 2]], [1, [2, 3]]]);
 });
