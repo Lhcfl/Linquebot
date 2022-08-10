be0399ef3e3c62295c7cdbd1e18f45fff76d2983
Lhcfl <lhcfl@outlook.com>
2022-08-10 10:04:07 +0800

    remove typescript

--- helper/BotMsg.ts
+++
@@ -1,7 +0,0 @@
-import { Sendable } from "oicq";
-
-/**
- * A message said by the bot. Can be either eager or lazy evaluated.
- * The first field is the message itself, and the second one is the delay of the message.
- */
-export type BotMsg = [Sendable, number | undefined] | (() => [Sendable, number | undefined]);
