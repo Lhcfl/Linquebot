9b6977216eb447967cd51600929a10053936f1df
Lhcfl <lhcfl@outlook.com>
2022-08-11 17:27:12 +0800

    fix some bugs

--- components/hitokoto.js
+++ components/hitokoto.js
@@ -1,6 +1,11 @@
 const api_addr = "https://v1.hitokoto.cn/?c="; //网址
 
 export async function get_hitokoto(arg = "") {
-    let res = await fetch(api_addr + arg);
-    return (await res.json())
+    try {
+        let res = await fetch(api_addr + arg);
+        return (await res.json());
+    } catch (error) {
+        return ({hitokoto: "网络错误", from: ""});
+    }
+    
 }
